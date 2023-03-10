de8ef820928ad1645d4da78e185f6cbf877af77f
IanFeugeas <ianfabricio1889@gmail.com>
2023-03-09 21:57:36 -0300

    Routes get terminada

--- controllers/mangas/get_mangas.js
+++ controllers/mangas/get_mangas.js
@@ -1,7 +1,19 @@
-import Manga from "../../models/Manga.js";
+import {Manga} from "../../models/Manga.js";
 
-const controller = {
+const getController = {
     read: async (req, res) => {
+
+        let order = { title: 1}
+        if(req.query.order == 1 || req.query.order == -1){
+            order.title = req.query.order
+        }
+
+        let pagination = { page: 1, limit: 6}
+        if(req.query.page){
+            pagination.page = Number(req.query.page)
+        }
+
+        let skip = pagination.page > 1 ? (pagination.page - 1) * pagination.limit: 0
 
         let query = {}
         if(req.query.title){
@@ -12,5 +24,22 @@ const controller = {
             query.category_id = req.query.categories.split(",")
         }
 
+        let mangas = await Manga.find(query)
+        .select("title category_id cover_photo _id")
+        .sort(order)
+        .skip(skip)
+        .limit( pagination.limit > 0 ? pagination.limit: 0)
+        .populate("category_id", "name -_id")
+
+        return res
+        .status(200)
+        .json({
+            success: true,
+            message: "aca estan todos los capitulos",
+            mangas
+        })
+
     }
 }
+
+export default getController
--- routes/manga.js
+++ routes/manga.js
@@ -5,13 +5,19 @@ import controller from "../controllers/mangas/create.js"
 import exist_title from "../middlewares/manga/exist_title.js"
 import allController from '../controllers/category/all.js'
 import passport from "../middlewares/passport.js";
+import getController from "../controllers/mangas/get_mangas.js"
+
+
 const {create} = controller
 const { all } = allController
+const { read } = getController
 
 let router = express.Router();
 
 
 router.post("/",passport.authenticate("jwt", {session: false}), validator(mangaCreate), exist_title, create)
+
 router.get('/', all)
+router.get("/", passport.authenticate('jwt', {session:false}), read)
 
 export default router;
