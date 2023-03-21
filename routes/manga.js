66543702e08180331fea4a02265684fee11c4c92
IanFeugeas <ianfabricio1889@gmail.com>
2023-03-21 16:03:57 -0300

    Testing 02 sobre mangas, terminado

--- routes/manga.js
+++ routes/manga.js
@@ -5,10 +5,12 @@ import controller from "../controllers/mangas/create.js"
 import exist_title from "../middlewares/manga/exist_title.js"
 import passport from "../middlewares/passport.js";
 import getController from "../controllers/mangas/get_mangas.js"
+import get_One from '../controllers/mangas/get_one.js'
 
 
 const { create } = controller
 const { read } = getController
+const { getOne } = get_One
 
 let router = express.Router();
 
@@ -16,5 +18,7 @@ let router = express.Router();
 router.post("/",passport.authenticate("jwt", {session: false}), validator(mangaCreate), exist_title, create)
 
 router.get("/", passport.authenticate('jwt', {session:false}), read)
+
+router.get('/:id', passport.authenticate('jwt', { session: false }), getOne)
 
 export default router;
