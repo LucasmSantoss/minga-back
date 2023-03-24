690ce7e937bc02a303cf2ceffc6f4e9dc1fbf1c1
IanFeugeas <ianfabricio1889@gmail.com>
2023-03-23 21:26:22 -0300

    fix

--- routes/manga.js
+++ routes/manga.js
@@ -1,16 +1,13 @@
 import express from "express";
-
 import mangaCreate from "../schemas/mangas.js";
 import validator from "../middlewares/validator.js"
 import controller from "../controllers/mangas/create.js"
 import exist_title from "../middlewares/manga/exist_title.js"
-
 import getOne from '../controllers/mangas/get_one.js'
 import get_controller from "../controllers/mangas/get_mangas.js"
 import passport from "../middlewares/passport.js";
 import updateMangas from '../controllers/mangas/update.js'
 import destroyMangas from '../controllers/mangas/destroy.js'
-import finds_id from '../middlewares/auth/finds_id.js'
 import updateMangaSchema from '../schemas/updateMangas.js'
 import getMeMangas from '../controllers/mangas/get_me.js'
 
@@ -19,7 +16,7 @@ const { get_one } = getOne
 const { get_me } = getMeMangas
 const { update } = updateMangas
 const { destroy } = destroyMangas
-const {read } = get_controller
+const { read } = get_controller
 
 
 
