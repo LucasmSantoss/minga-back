9290f4dabfc6aed7fb3e5a1575df60a7eaa71422
Lucas Matias Santos <lucasmsantoss95@hotmail.com>
2023-03-28 16:30:08 -0300

    m-16 agregada

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ import mangaRouter from './manga.js'
 import categoryRouter from "./category.js"
 import authorRouter from "./authors.js"
 import reactionsRouter from './reactions.js'
-
+import commentsRouter from './comments.js'
 
 
 let router = express.Router();
@@ -21,6 +21,7 @@ router.use('/chapters', chapterRouter)
 router.use('/',userRouter)
 router.use('/authors', authorRouter)
 router.use("/category", categoryRouter)
+router.use('/comments',commentsRouter)
 router.use('/reactions', reactionsRouter)
 router.use('/manga', mangaRouter)
 router.use("/category", categoryRouter)
